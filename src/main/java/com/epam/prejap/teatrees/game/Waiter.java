a6021f211134e753432e0b550c974246261e1dd2
KamKoz92 <51924117+KamKoz92@users.noreply.github.com>
2022-01-28 17:09:52 +0100

    Waiter javadoc update

--- src/main/java/com/epam/prejap/teatrees/game/Waiter.java
+++ src/main/java/com/epam/prejap/teatrees/game/Waiter.java
@@ -6,23 +6,37 @@ public class Waiter {
 
     private int milliseconds;
 
+    /**
+     * Constructs a newly allocated {@code Waiter} object with an internal
+     * delay value set to the value of the passed argument.
+     * 
+     * @param milliseconds internal delay time in milliseconds
+     * @since 0.1
+     */
     public Waiter(int milliseconds) {
         this.milliseconds = milliseconds;
     }
 
     /**
-     * Decreases the delay between cycles by 100 milliseconds every 10 points.
+     * Decreases the objects's delay value by 100 milliseconds every 10 points.
      * Minimum delay is capped at 100 milliseconds.
      * 
      * @param score game score
-     * @since 0.2
+     * @since 0.3
      */
     public void decreaseCycleDelay(int score) {
         if (score % 10 == 0 && milliseconds > 100) {
             milliseconds -= 100;
         }
+
     }
 
+    /**
+     * Causes to pause code execution for the time specified
+     * in the object's internal delay variable.
+     * 
+     * @since 0.1
+     */
     public void waitForIt() {
         try {
             TimeUnit.MILLISECONDS.sleep(milliseconds);
