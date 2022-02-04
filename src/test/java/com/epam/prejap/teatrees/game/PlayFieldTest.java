3c5f25756596b9ad99c92e37d04e396626b00019
KamKoz92 <51924117+KamKoz92@users.noreply.github.com>
2022-02-04 20:37:15 +0100

    improved PlayField tests

--- src/test/java/com/epam/prejap/teatrees/game/PlayFieldTest.java
+++ src/test/java/com/epam/prejap/teatrees/game/PlayFieldTest.java
@@ -3,42 +3,42 @@ package com.epam.prejap.teatrees.game;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 import com.epam.prejap.teatrees.block.BlockFeed;
 
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class PlayfieldTest {
-    private byte[][] expectedGridAfterFirstBlock;
-    private byte[][] expectedGridAfterKeyDown;
-    private byte[][] expectedGridAfterSecondBlock;
+    private Integer[] expectedGridAfterFirstBlock;
+    private Integer[] expectedGridAfterKeyDown;
+    private Integer[] expectedGridAfterSecondBlock;
     private ByteArrayOutputStream consoleContent = new ByteArrayOutputStream();
 
-    @BeforeTest
-    void BeforeTest() {
-        expectedGridAfterFirstBlock = new byte[][] {
-                { 0, 0, 1, 1, 0, 0, },
-                { 0, 0, 1, 1, 0, 0, },
-                { 0, 0, 0, 0, 0, 0, },
-                { 0, 0, 0, 0, 0, 0, },
-                { 0, 0, 0, 0, 0, 0, },
-        };
-        expectedGridAfterKeyDown = new byte[][] {
-                { 0, 0, 0, 0, 0, 0, },
-                { 0, 0, 0, 0, 0, 0, },
-                { 0, 0, 0, 0, 0, 0, },
-                { 0, 0, 1, 1, 0, 0, },
-                { 0, 0, 1, 1, 0, 0, },
-        };
-        expectedGridAfterSecondBlock = new byte[][] {
-                { 0, 0, 1, 1, 0, 0, },
-                { 0, 0, 1, 1, 0, 0, },
-                { 0, 0, 0, 0, 0, 0, },
-                { 0, 0, 1, 1, 0, 0, },
-                { 0, 0, 1, 1, 0, 0, },
-        };
+    PlayfieldTest() {
+        initializeGridsForBottomMove();
+    }
+
+    void initializeGridsForBottomMove() {
+        expectedGridAfterFirstBlock = new Integer[] {
+                0, 0, 1, 1, 0, 0,
+                0, 0, 1, 1, 0, 0,
+                0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0 };
+        expectedGridAfterKeyDown = new Integer[] {
+                0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0,
+                0, 0, 1, 1, 0, 0,
+                0, 0, 1, 1, 0, 0 };
+        expectedGridAfterSecondBlock = new Integer[] {
+                0, 0, 1, 1, 0, 0,
+                0, 0, 1, 1, 0, 0,
+                0, 0, 0, 0, 0, 0,
+                0, 0, 1, 1, 0, 0,
+                0, 0, 1, 1, 0, 0 };
     }
 
     @Test(groups = "bottomMove")
@@ -48,14 +48,27 @@ public class PlayfieldTest {
         SoftAssert sa = new SoftAssert();
 
         playfield.nextBlock();
-        sa.assertEquals(playfield.grid, expectedGridAfterFirstBlock);
+        byte[] arrayAfterFirstBlock = this.consoleContent.toByteArray();
+        Integer[] outAfterFirstBlock = IntStream.range(0, arrayAfterFirstBlock.length)
+                .mapToObj(b -> arrayAfterFirstBlock[b])
+                .filter(f -> f == ' ' || f == '#').map(i -> (i == ' ' ? 0 : 1)).toArray(Integer[]::new);
+        sa.assertEquals(outAfterFirstBlock, expectedGridAfterFirstBlock);
 
+        this.consoleContent.reset();
         playfield.downArrowPressed = true;
         playfield.move(Move.NONE);
-        sa.assertEquals(playfield.grid, expectedGridAfterKeyDown);
+        byte[] arrayAfterKeyDown = this.consoleContent.toByteArray();
+        Integer[] outAfterKeyDown = IntStream.range(0, arrayAfterKeyDown.length).mapToObj(b -> arrayAfterKeyDown[b])
+                .filter(f -> f == ' ' || f == '#').map(i -> (i == ' ' ? 0 : 1)).toArray(Integer[]::new);
+        sa.assertEquals(outAfterKeyDown, expectedGridAfterKeyDown);
 
+        this.consoleContent.reset();
         playfield.nextBlock();
-        sa.assertEquals(playfield.grid, expectedGridAfterSecondBlock);
+        byte[] arrayAfterSecondBlock = this.consoleContent.toByteArray();
+        Integer[] outAfterSecondBlock = IntStream.range(0, arrayAfterSecondBlock.length)
+                .mapToObj(b -> arrayAfterSecondBlock[b])
+                .filter(f -> f == ' ' || f == '#').map(i -> (i == ' ' ? 0 : 1)).toArray(Integer[]::new);
+        sa.assertEquals(outAfterSecondBlock, expectedGridAfterSecondBlock);
 
         sa.assertAll();
     }
@@ -66,5 +79,4 @@ public class PlayfieldTest {
             return 0;
         }
     }
-
 }
